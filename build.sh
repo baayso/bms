5029580ac2c10cc7e59973fbb59133b7dea7bed9
baayso <baayso@outlook.com>
2018-12-05 14:57:42 +0800

    新增构建脚本。

---
+++ build.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "[Pre-Requirement] Makesure install JDK 8.0+ and set the JAVA_HOME."
+echo "[Pre-Requirement] Makesure install Maven 3.2+ and set the PATH."
+
+MVN="mvn"
+
+${MVN} clean package -Dmaven.test.skip=true
